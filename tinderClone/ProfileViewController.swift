c56907653ead155831c9d43cff4b9faa33fa3e1d
dougwells <dougwells@gmail.com>
2017-01-16 14:39:52 -0700

    tinderClone. Add ability for user to save image, gender & orientation to Parse

--- tinderClone/ProfileViewController.swift
+++ tinderClone/ProfileViewController.swift
@@ -15,19 +15,33 @@ class ProfileViewController: UIViewController, UINavigationControllerDelegate, U
     var genderMale = true
     var interestMale = false
     var isGay = false
+    var defaultImage = true
+    var maleImage = UIImage(named: "cruiseCartoon.jpg")
+    var femaleImage = UIImage(named: "megCartoon.jpg")
+
     
     //declare activityIndicator (needed for functions start/stop spinner)
     var activityIndicator = UIActivityIndicatorView.init(frame: CGRect(x: 0, y: 300, width: 100, height: 100))
     
     @IBOutlet weak var imageToPost: UIImageView!
     
+    
     @IBAction func genderMaleSwitch(_ sender: UISwitch) {
         if sender.isOn {
             genderMale = false
             print("user is a woman")
+            if defaultImage {
+                imageToPost.image = femaleImage
+            }
+            
         } else {
+            
             genderMale = true
             print("user is man")
+            if defaultImage {
+               imageToPost.image = maleImage
+            }
+            
         }
         gay()
     }
@@ -79,6 +93,7 @@ class ProfileViewController: UIViewController, UINavigationControllerDelegate, U
             
             //Updates imageView on Main.storyboard (see IBOutlet below)
             imageToPost.image = image
+            defaultImage = false
             
         } else {
             print("There was a problem getting the image")
@@ -90,7 +105,75 @@ class ProfileViewController: UIViewController, UINavigationControllerDelegate, U
 
     @IBAction func updateProfile(_ sender: UIButton) {
         
+        self.startSpinner()
+        let tinderProfiles = PFObject(className: "tinderProfiles")
+        tinderProfiles["userid"] = user?.objectId
+        tinderProfiles["genderMale"] = genderMale
+        tinderProfiles["interestMale"] = interestMale
+        tinderProfiles["isGay"] = isGay
+        
+        
+        let imageData = UIImageJPEGRepresentation(imageToPost.image!, 0.5)
+        let imageFile = PFFile(name: "image.jpeg", data: imageData!)
+        tinderProfiles["userImage"] = imageFile
+        
+        tinderProfiles.saveInBackground { (success, error) in
+            self.stopSpinner()
+            if error != nil {
+                self.createAlert(title: "Error Saving Image", message: "Please try again later.  Thanks")
+            } else {
+                
+                self.createAlert(title: "Successfull", message: "Your image profile has been saved")
+                
+                self.defaultImage = true
+                
+                if self.genderMale {
+                    
+                  self.imageToPost.image = self.maleImage
+                    
+                } else {
+                    
+                    self.imageToPost.image = self.femaleImage
+                    
+                }
+                
+            }
+        }
+    } //end updateProfile
+    
+    
+    
+    func createAlert(title: String, message: String ) {
+        //creat alert
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+        
+        //add button to alert
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) in
+            alert.dismiss(animated: true, completion: nil)
+        }))
+        
+        //present alert
+        self.present(alert, animated: true, completion: nil)
     }
+    
+    func startSpinner(){
+        
+        
+        activityIndicator.center = self.view.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray
+        view.addSubview(activityIndicator)
+        activityIndicator.startAnimating()
+        UIApplication.shared.beginIgnoringInteractionEvents()
+        
+    }
+    
+    func stopSpinner(){
+        activityIndicator.stopAnimating()
+        UIApplication.shared.endIgnoringInteractionEvents()
+    }
+
+
 
     
     
