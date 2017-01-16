724bbe11e523cf3c19171fe38d94e7b97b025d18
dougwells <dougwells@gmail.com>
2017-01-16 08:29:08 -0700

    tinderClone. Add image to login page

--- tinderClone/ViewController.swift
+++ tinderClone/ViewController.swift
@@ -11,37 +11,128 @@ import UIKit
 import Parse
 
 class ViewController: UIViewController {
+    
+    var signupMode = true
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var changeSignupModeButton: UIButton!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var signupOrLoginButton: UIButton!
+    
+    let activityIndicator = UIActivityIndicatorView.init(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+    
+    
+    @IBAction func changeSignupMode(_ sender: Any) {
+        
+        if signupMode {
+            //Change layout to login
+            
+            signupOrLoginButton.setTitle("Log In", for: [])
+            messageLabel.text = "Don't have an account?"
+            changeSignupModeButton.setTitle("Sign Up", for: [])
+            
+        } else {
+            signupOrLoginButton.setTitle("Sign Up", for: [])
+            messageLabel.text = "Already have an account?"
+            changeSignupModeButton.setTitle("Log In", for: [])
+        }
+        signupMode = !signupMode
+        
+    }
+    
+    @IBAction func signupOrLogin(_ sender: Any) {
+        
+        if emailTextField.text == "" || passwordTextField.text == "" {
+            
+            createAlert(title: "Error in form", message: "Please enter both username and password")
+            
+        } else {
+            startSpinner()
+            if signupMode {
+                // Save user in Parse
+                let user = PFUser()
+                user.username = emailTextField.text
+                user.password = passwordTextField.text
+                user.signUpInBackground { (success, error) -> Void in
+                    self.stopSpinner()
+                    if success {
+                        print("New user \(user.username!) saved")
+                        
+                        self.createAlert(title: "Success!", message: "Welcome \(user.username!)")
+                        //self.performSegue(withIdentifier: "showUserTable", sender: self)
+                    } else {
+                        if error != nil {
+                            print("Error saving user", error)
+                            var displayErrorMessage = "Please try again later ..."
+                            if let errorMessage = error as NSError? {
+                                displayErrorMessage = errorMessage.userInfo["error"] as! String
+                            }
+                            self.createAlert(title: "Signup Error", message: displayErrorMessage)
+                        }
+                    }
+                }
+            } else {    //Login mode
+                PFUser.logInWithUsername(inBackground: emailTextField.text!, password: passwordTextField.text!, block: { (user, error) in
+                    self.stopSpinner()
+                    if (user != nil) {
+                        
+                    self.createAlert(title: "Success!", message: "Welcome back \(user!.username!)")
+                        print("Existing user logged in", user!.username!)
+                        //self.performSegue(withIdentifier: "showUserTable", sender: self)
+
+                    }
+                    if error != nil {
+                        print("Error logging in existing user", error)
+                        var displayErrorMessage = "Please try again later ..."
+                        if let errorMessage = error as NSError? {
+                            displayErrorMessage = errorMessage.userInfo["error"] as! String
+                        }
+                        self.createAlert(title: "Login Error", message: displayErrorMessage)
+                    }
+                })
+            }
+            
+        }
+    }
+    
+    
+    
+    
+    func startSpinner(){
+        activityIndicator.center = self.view.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.gray
+        view.addSubview(activityIndicator)
+        activityIndicator.startAnimating()
+        UIApplication.shared.beginIgnoringInteractionEvents()
+        
+    }  //End startSpinner
+    
+    func stopSpinner(){
+        activityIndicator.stopAnimating()
+        UIApplication.shared.endIgnoringInteractionEvents()
+    }
+    
+    func createAlert(title: String, message: String ) {
+        //creat alert
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
+        
+        //add button to alert
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) in
+            self.dismiss(animated: true, completion: nil)
+        }))
+        
+        //present alert
+        self.present(alert, animated: true, completion: nil)
+    }  //End createAlert
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let testObject = PFObject(className: "TestObject2")
-        
-        testObject["foo"] = "bar"
-        
-        testObject.saveInBackground { (success, error) -> Void in
-            
-            // added test for success 11th July 2016
-            
-            if success {
-                
-                print("Object has been saved.")
-                
-            } else {
-                
-                if error != nil {
-                    
-                    print (error)
-                    
-                } else {
-                    
-                    print ("Error")
-                }
-                
-            }
-            
-        }
+
         
     }
 
